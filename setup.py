87c4ef762564599c59df2ee89ceabcfec5545bd9
Anas <anassiddiqui634@gmail.com>
2024-08-07 04:43:11 +0530

    completepipeline directories

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+setup(
+name="src",
+version="0.0.1",
+author="Anas",
+author_email="moanas.n@pw.live",
+packages=find_packages(),
+install_requires=[],
+)
